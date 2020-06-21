d6ce72932aefdfb1bd193691a18fe1215a568ae9
Rohman Habib M <mhrohman@live.com>
2020-06-21 18:09:55 +0700

    feat(example): add with-tailwindcss

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from 'next/app'
+import React from 'react'
+import '../styles.scss'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
